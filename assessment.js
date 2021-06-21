58578abc0fd56ea03cfaa6e11bb9a4810f5d40e8
asika-2005 <86187497+asika-2005@users.noreply.github.com>
2021-06-21 14:35:15 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -4,13 +4,10 @@ const assessmentButton = document.getElementById('assessment');
 const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
 
-/**
- * 指定した要素の子どもを全て除去する
- * @param {HTMLElement} element HTMLの要素
- */
+
 function removeAllChildren(element) {
   while (element.firstChild) {
-    // 子どもの要素があるかぎり除去
+
     element.removeChild(element.firstChild);
   }
 }
@@ -18,11 +15,11 @@ function removeAllChildren(element) {
 assessmentButton.onclick = () => {
   const userName = userNameInput.value;
   if (userName.length === 0) {
-    // 名前が空の時は処理を終了する
+   
     return;
   }
 
-  // 診断結果表示エリアの作成
+
   removeAllChildren(resultDivided);
   const header = document.createElement('h3');
   header.innerText = '診断結果';
@@ -33,7 +30,7 @@ assessmentButton.onclick = () => {
   paragraph.innerText = result;
   resultDivided.appendChild(paragraph);
 
-  // ツイートエリアの作成
+
   removeAllChildren(tweetDivided);
   const anchor = document.createElement('a');
   const hrefValue =
@@ -46,7 +43,7 @@ assessmentButton.onclick = () => {
   anchor.innerText = 'Tweet #あなたのいいところ';
   tweetDivided.appendChild(anchor);
 
-  // widgets.js の設定
+
   const script = document.createElement('script');
   script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
   tweetDivided.appendChild(script);
@@ -71,19 +68,13 @@ const answers = [
   '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
 ];
 
-/**
- * 名前の文字列を渡すと診断結果を返す関数
- * @param {string} userName ユーザーの名前
- * @return {string} 診断結果
- */
 function assessment(userName) {
-  // 全文字のコード番号を取得してそれを足し合わせる
+
   let sumOfcharCode = 0;
   for (let i = 0; i < userName.length; i++) {
     sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
   }
 
-  // 文字のコード番号の合計を回答の数で割って添字の数値を求める
   const index = sumOfcharCode % answers.length;
   let result = answers[index];
 
@@ -91,7 +82,7 @@ function assessment(userName) {
   return result;
 }
 
-// テストコード
+
 console.assert(
   assessment('太郎') ===
     '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
